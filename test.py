0cefe0a9121fa5c9570cfcdad887cd8ee2c65c43
mahesh <mahesh.mcp12@gmail.com>
2023-11-19 22:39:38 +0530

    automation script changes

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 url ="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
 
-df = pd.read_parquet(url)
+df = pd.read_parquet(url,iterator=True, chunksize=100000)
+
 
 print (df.head(100))
